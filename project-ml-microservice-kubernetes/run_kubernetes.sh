7ba4f393ad2c65fea59d46bd79a7656a17968e4e
Marietta <akumbom5ma@gmail.com>
2023-03-04 04:00:58 +0100

    setting up kubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=marietta95/project-ml:v1.0.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy project-ml-pod --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
+#get pod name
+pod=$(kubectl get pods -o name)
+echo pod: $pod
 # Forward the container port to a host
-
+kubectl port-forward $pod --address 0.0.0.0 8000:80
